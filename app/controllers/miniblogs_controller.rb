05fd8d93ddb304468d8f4c02c34c6e0392519670
EC2 Default User <ec2-user@ip-172-31-19-186.ap-northeast-1.compute.internal>
2021-08-20 05:20:28 +0000

    forth commit

--- app/controllers/miniblogs_controller.rb
+++ app/controllers/miniblogs_controller.rb
@@ -1,4 +1,5 @@
 class MiniblogsController < ApplicationController
+  before_action :set_miniblog, only: [:edit, :show]
     
     def index
       @miniblogs = Miniblog.all
@@ -11,25 +12,34 @@ class MiniblogsController < ApplicationController
     
     def create
       Miniblog.create(miniblog_params)
+      redirect_to root_path
     end
     
     def destroy
       miniblog = Miniblog.find(params[:id])
       miniblog.destroy
+      redirect_to root_path
     end
     
     def edit
-      @miniblog = Miniblog.find(params[:id])
     end
     
     def update
       miniblog = Miniblog.find(params[:id])
       miniblog.update(miniblog_params)
+      redirect_to root_path
+    end
+    
+    def show
     end
     
     private #以下はプライベートメソッドを定義しています
     
     def miniblog_params
       params.require(:miniblog).permit(:text)
+    end
+    
+    def set_miniblog
+      @miniblog = Miniblog.find(params[:id])
     end
 end
